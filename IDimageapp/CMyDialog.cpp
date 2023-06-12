63a93e3121dca69039df89bbc4bf661e64da08b3
Shuyer0377 <128669106+Shuyer0377@users.noreply.github.com>
2023-06-13 01:55:59 +0800

    lesson5

---
+++ IDimageapp/CMyDialog.cpp
@@ -0,0 +1,34 @@
+﻿// CMyDialog.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "IDimageapp.h"
+#include "afxdialogex.h"
+#include "CMyDialog.h"
+
+
+// CMyDialog 对话框
+
+IMPLEMENT_DYNAMIC(CMyDialog, CDialogEx)
+
+CMyDialog::CMyDialog(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_IDIMAGEAPP_DIALOG, pParent)
+{
+
+}
+
+CMyDialog::~CMyDialog()
+{
+}
+
+void CMyDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CMyDialog, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMyDialog 消息处理程序
