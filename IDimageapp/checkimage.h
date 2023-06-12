7446fcf5089712c7d4f216553545761222a95139
Shuyer0377 <128669106+Shuyer0377@users.noreply.github.com>
2023-06-13 01:51:41 +0800

    Add files via upload
    lesson5

---
+++ IDimageapp/checkimage.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// checkimage 对话框
+
+class checkimage : public CDialogEx
+{
+	DECLARE_DYNAMIC(checkimage)
+
+public:
+	checkimage(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~checkimage();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_IDIMAGEAPP_DIALOG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
